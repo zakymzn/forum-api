2e25d4ed1370242492ab884d4f80e2db33617688
zakymzn <mamurzakynurrokhman@gmail.com>
2025-11-27 23:49:15 +0700

    fix body response

--- src/Applications/use_case/GetThreadUseCase.js
+++ src/Applications/use_case/GetThreadUseCase.js
@@ -1,12 +1,24 @@
 class GetThreadUseCase {
-  constructor({ threadRepository, commentRepository }) {
+  constructor({ threadRepository, commentRepository, replyRepository }) {
     this._threadRepository = threadRepository;
     this._commentRepository = commentRepository;
+    this._replyRepository = replyRepository;
   }
 
   async execute(threadId) {
     const thread = await this._threadRepository.getThreadById(threadId);
     const comments = await this._commentRepository.getCommentsByThreadId(threadId);
+
+    // fetch replies for each comment
+    const commentsWithReplies = await Promise.all(
+      comments.map(async (comment) => {
+        const replies = await this._replyRepository.getReplyByCommentId(comment.id);
+        return {
+          ...comment,
+          replies,
+        };
+      }),
+    );
 
     return {
       id: thread.id,
@@ -14,7 +26,7 @@ class GetThreadUseCase {
       body: thread.body,
       date: thread.created_at,
       username: thread.username,
-      comments,
+      comments: commentsWithReplies,
     };
   }
 }
